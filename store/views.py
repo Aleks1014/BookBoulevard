b5e7060602da98847baa4d65277c11e34e13eb22
Aleks <aleks.dimova96@gmail.com>
2024-05-01 22:49:04 +0100

    Register form created

--- store/views.py
+++ store/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 
 # Create your views here.
 def home(request):
     products = Product.objects.all()[:4]
-    return render(request,'home.html', {'products': products})
+    return render(request, 'home.html', {'products': products})
 
 
 def about(request):
@@ -34,3 +38,26 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'Successfully logout.')
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have registered successfully.')
+            return redirect('home')
+        else:
+            messages.error(request, 'There was a problem registering. Please try again.')
+            return redirect('register')
+
+    else:
+        return render(request, 'register.html', {'form': form})
+
+
