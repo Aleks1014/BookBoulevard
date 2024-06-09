76f958971a23f36fac85bf759c3b1fb64b7dd77a
Aleks <aleks.dimova96@gmail.com>
2024-06-09 17:29:11 +0100

    Order processing

--- order_process/views.py
+++ order_process/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
 from order_process.forms import ShippingForm, PaymentForm
-from order_process.models import ShippingAddress
+from order_process.models import ShippingAddress, Order, OrderItem
 from django.contrib import messages
+from django.contrib.auth.models import User
+from store.models import Product
 
 
 # Create your views here.
@@ -20,16 +22,14 @@ def checkout(request):
     if request.user.is_authenticated:
         shipping_user = ShippingAddress.objects.get(user_id=request.user.id)
         shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
-        return render(request, 'payment/checkout.html', {'cart_products': cart_products,
-                                                         'quantities': quantities,
-                                                         'total_price': total_price,
-                                                         'shipping_form': shipping_form})
+        return render(request, 'payment/checkout.html',
+                      {'cart_products': cart_products, 'quantities': quantities, 'total_price': total_price,
+                       'shipping_form': shipping_form})
     else:
         shipping_form = ShippingForm(request.POST or None)
-        return render(request, 'payment/checkout.html', {'cart_products': cart_products,
-                                                         'quantities': quantities,
-                                                         'total_price': total_price,
-                                                         'shipping_form': shipping_form})
+        return render(request, 'payment/checkout.html',
+                      {'cart_products': cart_products, 'quantities': quantities, 'total_price': total_price,
+                       'shipping_form': shipping_form})
 
 
 def billing_info(request):
@@ -38,22 +38,79 @@ def billing_info(request):
         cart_products = cart.get_products
         quantities = cart.get_quants
         total_price = cart.total()
-        shipping_form = request.POST
+        # session with shipping info
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
         if request.user.is_authenticated:
             billing_form = PaymentForm()
-            return render(request, 'payment/billing_info.html', {'cart_products': cart_products,
-                                                                 'quantities': quantities,
-                                                                 'total_price': total_price,
-                                                                 'shipping_info': request.POST,
-                                                                 'billing_form': billing_form})
+            return render(request, 'payment/billing_info.html',
+                          {'cart_products': cart_products, 'quantities': quantities, 'total_price': total_price,
+                           'shipping_info': request.POST, 'billing_form': billing_form})
         else:
             billing_form = PaymentForm()
-            return render(request, 'payment/billing_info.html', {'cart_products': cart_products,
-                                                                 'quantities': quantities,
-                                                                 'total_price': total_price,
-                                                                 'shipping_info': request.POST,
-                                                                 'billing_form': billing_form})
+            return render(request, 'payment/billing_info.html',
+                          {'cart_products': cart_products, 'quantities': quantities, 'total_price': total_price,
+                           'shipping_info': request.POST, 'billing_form': billing_form})
+
 
     else:
         messages.error(request, 'Access Denied')
+        return redirect('home')
+
+
+def process_order(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_products
+        quantities = cart.get_quants
+        total_price = cart.total()
+        payment_form = PaymentForm(request.POST or None)
+        my_shipping = request.session.get('my_shipping')
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        amount_paid = total_price
+        full_shipping_address = f'{my_shipping["shipping_address_1"]}\n' \
+                                f'{my_shipping["shipping_address_2"]}\n' \
+                                f'{my_shipping["shipping_city"]}\n' \
+                                f'{my_shipping["shipping_postal_code"]}' \
+                                f'\n{my_shipping["shipping_country"]}\n'
+
+        if request.user.is_authenticated:
+            user = request.user
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address=full_shipping_address,
+                                 amount_paid=amount_paid)
+            create_order.save()
+            order_id = create_order.pk
+            for product in cart_products():
+                product_id = product.id
+                if product.is_sale:
+                    product_price = product.sale_price
+                else:
+                    product_price = product.price
+                product_quantity = quantities()[str(product_id)]
+                create_order_item = OrderItem(order_id=order_id, product_id=product_id,user=user,
+                                              quantity=product_quantity,
+                                              price=product_price)
+                create_order_item.save()
+
+        else:
+            create_order = Order(full_name=full_name, email=email, shipping_address=full_shipping_address,
+                                 amount_paid=amount_paid)
+            create_order.save()
+            order_id = create_order.pk
+            for product in cart_products():
+                product_id = product.id
+                if product.is_sale:
+                    product_price = product.sale_price
+                else:
+                    product_price = product.price
+                product_quantity = quantities()[str(product_id)]
+                create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=product_quantity,
+                                              price=product_price)
+                create_order_item.save()
+        messages.success(request, 'Order Placed.')
+        return redirect('home')
+
+    else:
+        messages.error(request, 'Access Denied.')
         return redirect('home')
